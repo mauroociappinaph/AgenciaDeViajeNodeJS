7902d2b2c21ea3145a370e5f49aa7129da151459
mauroociappinaph <ciappinamauroo@gmail.com>
2023-03-30 12:48:03 -0300

    Visualizar todas las paginas

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,19 @@
-import  express  from "express";
+import express from "express";
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.render("inicio", {
+    pagina: "inicio",
+  });
+});
 
-router.get('/', (req, res) =>{
-    res.render("inicio" , {
-        
-    })
-})
-router.get('/nosotros', (req, res) =>{
+router.get("/nosotros", (req, res) => {
+  const viajes = "Viaje a Brasil";
 
-const viajes = "Viaje a Brasil"
-
-
-    res.render("nosotros" , {
-        viajes
-    })
-   
-})
-
+  res.render("nosotros", {
+    viajes,
+  });
+});
 
 export default router;
