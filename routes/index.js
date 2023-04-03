fe235ab36609a41d718e1e6366a0b4724a6d51ea
MAURO CIAPPINA <100035249+mauroociappinaph@users.noreply.github.com>
2023-04-03 18:57:00 -0300

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,26 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   res.render("inicio", {
-    pagina: "inicio",
+    pagina: "Inicio",
   });
 });
 
 router.get("/nosotros", (req, res) => {
-  const viajes = "Viaje a Brasil";
-
   res.render("nosotros", {
-    viajes,
+    pagina: "Nosotros",
+  });
+});
+
+router.get("/viajes", (req, res) => {
+  res.render("viajes", {
+    pagina: "Viajes",
+  });
+});
+
+
+router.get("/testimoniales", (req, res) => {
+  res.render("testimoniales", {
+    pagina: "Testimoniales",
   });
 });
 
