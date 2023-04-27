747a93169b25dddcfa68478074ed1b8fd8e905e1
MAURO CIAPPINA <100035249+mauroociappinaph@users.noreply.github.com>
2023-04-27 18:36:49 -0300

    Add files via upload
    Conexión de la base de datos

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
-import express from "express";
-import router from "./routes/index.js";
+import express from 'express';
+import router from './routes/index.js';
+import db from './config/db.js';
 
 const app = express();
 
-//Definir Puerto
-const port = process.env.PORT || 4000;
+// Conectar la base de datos
+db.authenticate()
+    .then( () => console.log('Base de datos conectada') )
+    .catch( error => console.log(error));
 
-//Habilitar PUG
-app.set("view engine", "pug");
 
-//? Pug es un motor de plantillas que simplifica la
-//? creación de vistas HTML dinámicas en una aplicación web,
-//? y es utilizado por muchos desarrolladores de Node.js y
-//? Express para generar vistas atractivas y fácilmente mantenibles.
+// Definir puerto
+const port = process.env.PORT || 3000;
+
+// Habilitar PUG
+app.set('view engine', 'pug');
 
 // Obtener el año actual
-app.use((req, res, next) => {
-  const year = new Date();
-
-  res.locals.actualYear = year.getFullYear();
-  res.locals.nombresitio = "Agencia de Viajes";
-  return next();
+app.use( (req, res, next) => {
+    const year = new Date();
+    res.locals.actualYear = year.getFullYear();
+    res.locals.nombresitio = "Agencia de Viajes";
+    next();
 });
 
-//Definir carpeta public
-app.use(express.static("public"));
+
+// Habilitar express.json
+app.use(express.urlencoded({ extended: false }));
+
+
+// Definir la carpeta publica
+app.use(express.static('public'));
 
 // Agregar Router
-app.use("/", router);
+app.use('/', router);
+
 
 app.listen(port, () => {
-  console.log(`El servidor está funcionando en el ${port}`);
-});
+    console.log(`El Servidor esta funcionando en el puerto ${port}`)
+})
