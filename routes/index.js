2ee0360e606bcecf50d1272342688a51411728c8
MAURO CIAPPINA <100035249+mauroociappinaph@users.noreply.github.com>
2023-04-27 19:20:59 -0300

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,29 @@
-import express from "express";
+import express from 'express';
+import { 
+    paginaInicio, 
+    paginaNosotros, 
+    paginaViajes, 
+    paginaTestimoniales, 
+    paginaDetalleViaje 
+} from '../controllers/paginasController.js';
+
+import { guardarTestimonial } from '../controllers/testimonialesController.js';
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("inicio", {
-    pagina: "Inicio",
-  });
-});
+router.get('/', paginaInicio);
 
-router.get("/nosotros", (req, res) => {
-  res.render("nosotros", {
-    pagina: "Nosotros",
-  });
-});
+router.get('/nosotros', paginaNosotros);
 
-router.get("/viajes", (req, res) => {
-  res.render("viajes", {
-    pagina: "Viajes",
-  });
-});
+router.get('/viajes', paginaViajes);
+
+router.get('/viajes/:slug', paginaDetalleViaje);
+
+router.get('/testimoniales', paginaTestimoniales);
 
 
-router.get("/testimoniales", (req, res) => {
-  res.render("testimoniales", {
-    pagina: "Testimoniales",
-  });
-});
+// Cuando Lleno el form
+router.post('/testimoniales', guardarTestimonial);
+
 
 export default router;
