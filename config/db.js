fb0f930d846905452b7f55bede18e209de20dde1
mauroociappinaph <ciappinamauroo@gmail.com>
2023-05-14 14:30:55 -0300

    Proyecto Listo Para Deployment

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv/config.js";
 
-console.log();
+
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  port: "3306",
+  port: process.env.DB_PORT,
   dialect: "mysql",
   define: {
     timestamps: false,
