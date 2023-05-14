9d693d10c440ee0b366a1762cdf7b17511af9574
MAURO CIAPPINA <100035249+mauroociappinaph@users.noreply.github.com>
2023-05-14 13:11:55 -0300

    Add files via upload
    Variables de entorno de HOST ,NAME y PASSWORD

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv/config.js";
 
-console.log(process.env.DB_HOST);
+console.log();
 
-const db = new Sequelize("agenciadeviajes", "root", "", {
-  host: "127.0.0.1",
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   port: "3306",
   dialect: "mysql",
   define: {
